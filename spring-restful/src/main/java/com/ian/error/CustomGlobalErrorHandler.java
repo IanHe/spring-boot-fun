0b66732ac3f4059f39286e327a8738c081d95888
IanHe <yinghe.professional@gmail.com>
2021-07-10 17:23:30 +1000

    add error handling

--- spring-restful/src/main/java/com/ian/error/CustomGlobalErrorHandler.java
+++ spring-restful/src/main/java/com/ian/error/CustomGlobalErrorHandler.java
@@ -1,5 +1,6 @@
 package com.ian.error;
 
+import com.ian.exceptions.BookNotFoundException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +13,7 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.ConstraintViolationException;
 import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -20,6 +22,21 @@ import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class CustomGlobalErrorHandler extends ResponseEntityExceptionHandler {
+
+    // Let Spring BasicErrorController handle the exception, we just override the status code
+    @ExceptionHandler(BookNotFoundException.class)
+    public void springHandleNotFound(HttpServletResponse response) throws IOException {
+        response.sendError(HttpStatus.NOT_FOUND.value());
+    }
+
+//    @ExceptionHandler(BookNotFoundException.class)
+//    public ResponseEntity<CustomErrorResponse> customHandleNotFound(Exception ex, WebRequest request) {
+//        CustomErrorResponse errors = new CustomErrorResponse();
+//        errors.setTimestamp(LocalDateTime.now());
+//        errors.setError(ex.getMessage());
+//        errors.setStatus(HttpStatus.NOT_FOUND.value());
+//        return new ResponseEntity<>(errors, HttpStatus.NOT_FOUND);
+//    }
 
     // @Validated on Controller for Validating Path Variables and Request Parameters
     @ExceptionHandler(ConstraintViolationException.class)
@@ -34,18 +51,18 @@ public class CustomGlobalErrorHandler extends ResponseEntityExceptionHandler {
             HttpHeaders headers,
             HttpStatus status,
             WebRequest request) {
-            Map<String, Object> body = new LinkedHashMap<>();
-            body.put("timestamp", new Date());
-            body.put("status", status.value());
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", new Date());
+        body.put("status", status.value());
 
-            //Get all errors
-            List<String> errors = ex.getBindingResult()
-                    .getFieldErrors()
-                    .stream()
-                    .map(x -> x.getDefaultMessage())
-                    .collect(Collectors.toList());
-            body.put("errors", errors);
-            return new ResponseEntity<>(body, headers, status);
+        //Get all errors
+        List<String> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(x -> x.getDefaultMessage())
+                .collect(Collectors.toList());
+        body.put("errors", errors);
+        return new ResponseEntity<>(body, headers, status);
     }
 
 }
